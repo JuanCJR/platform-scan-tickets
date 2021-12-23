3b85db4dceaac34f58a87c8dc133c9b69328e324
Juan C Jimenez <jcjimenezr@falabella.cl>
2021-12-23 18:32:03 -0300

    update

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ../bff-scan-tickets/
-git pull 
-docker build -t bff-scan-tickets .
-cd ..
-cd fe-scan-tickets 
-git pull
-docker build -t fe-scan-tickets .
-
--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+cd ../bff-scan-tickets/
+git pull 
+#·docker build -t bff-scan-tickets .
+cd ..
+cd fe-scan-tickets 
+git pull
+#docker build -t fe-scan-tickets .
+cd ..
+cd platform-scan-tickets
+
 docker-compose up --build -d --remove-orphans
